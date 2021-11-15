b09514cedefd384e18815b41b1c0d41addcca421
DWNeo <73509108+DWNeo@users.noreply.github.com>
2021-11-15 03:35:35 -0300

    Grande atualização

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,17 +1,24 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, url_for, request, flash, redirect, Blueprint
+from flask_login import current_user, login_required
 from flaskblog.models import Post
+from flaskblog.posts.forms import PostForm
 
 main = Blueprint('main', __name__)
 
 
 @main.route("/")
 @main.route("/home")
-def home():
+def home():  
+    if not current_user.is_authenticated:
+        flash('É necessário realizar login para utilizar o aplicativo.', 'info')
+        return redirect(url_for('users.login'))
+    form = PostForm()
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=1000)
+
+    return render_template('home.html', posts=posts, form=form)
 
 
 @main.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template('about.html', title='Sobre o Aplicativo')
